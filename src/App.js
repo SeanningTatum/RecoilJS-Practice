8f0385c6ef734816daf1808c24d4558c775be31c
Sean Stuart Urgel <seantheurgel@gmail.com>
2020-06-27 13:01:36 +0800

    hello world

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mt-5 justify-content-center">
+          <div className="col-md-8 d-flex align-items-center flex-column">
+            {/* <TodoList></TodoList> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
